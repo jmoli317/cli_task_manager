17aa943fb8ed6d0cd898c1aba3041165c86c8f41
Jacob <jmoliver317@gmail.com>
2025-11-29 23:25:11 -0500

    Add method skeletons for TaskManager class

--- task_manager.py
+++ task_manager.py
@@ -12,3 +12,18 @@ class TaskManager:
                 json.dump([], file)
 
         self.tasks = json.load(open(self.file_name))
+
+    def list_tasks(self):
+        pass
+
+    def save_tasks(self):
+        pass
+
+    def add_task(self):
+        pass
+
+    def edit_task(self):
+        pass
+
+    def delete_task(self):
+        pass
